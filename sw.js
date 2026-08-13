a9ae064bd1ea99fa46bf8c10d8b88106ad8b4f50
shazmeronly <shazseikui@gmail.com>
2026-08-13 12:21:22 +0800

    Patch fix calendar

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "crewview-completed-3h-v106";
+const CACHE = "crewview-v107";
 const PDF_MAIN = "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.5.136/pdf.min.mjs";
 const PDF_WORKER = "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.5.136/pdf.worker.min.mjs";
 const ASSETS = [
