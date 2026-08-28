ea042201e957a970fd86bb19c3e34b3a288db981
shazmeronly <shazseikui@gmail.com>
2026-08-28 23:30:18 +0800

    Polish PDF download

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE = "crewview-v155";
+const CACHE = "crewview-v156";
 const PDF_MAIN = "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.5.136/pdf.min.mjs";
 const PDF_WORKER = "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.5.136/pdf.worker.min.mjs";
 const ASSETS = [
   "./",
   "./index.html",
-  "./style.css?v=155",
-  "./app.js?v=155",
+  "./style.css?v=156",
+  "./app.js?v=156",
   "./airport-timezones.js",
   "./crewview-mark.svg",
   "./crewview-mark-final.png",
