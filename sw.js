ba374c5f75e28fedd67965ed7512f40e80cd2658
shazmeronly <shazseikui@gmail.com>
2026-08-29 01:47:20 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE = "crewview-v170";
+const CACHE = "crewview-v171";
 const PDF_MAIN = "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.5.136/pdf.min.mjs";
 const PDF_WORKER = "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.5.136/pdf.worker.min.mjs";
 const ASSETS = [
   "./",
   "./index.html",
-  "./style.css?v=170",
-  "./app.js?v=170",
+  "./style.css?v=171",
+  "./app.js?v=171",
   "./airport-timezones.js",
   "./crewview-mark.svg",
   "./crewview-mark-final.png",
